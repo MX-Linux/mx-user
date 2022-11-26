1ec4ab173cd7664998b5052fa3e99dfa5a9fa875
Adrian <adrian@mxlinux.org>
2022-11-26 17:15:03 -0500

    Style fixes

--- mainwindow.h
+++ mainwindow.h
@@ -28,7 +28,8 @@
 
 #include "cmd.h"
 
-class MainWindow : public QDialog, public Ui::MEConfig {
+class MainWindow : public QDialog, public Ui::MEConfig
+{
     Q_OBJECT
 
 public:
@@ -57,7 +58,7 @@ public:
     void refreshMembership();
     void refreshOptions();
     void refreshRename();
-    static void restartPanel(const QString &user);
+    static void restartPanel(const QString& user);
 
 public slots:
     void progress();
@@ -83,7 +84,7 @@ private slots:
     void on_entireRadioButton_clicked();
     void on_fromUserComboBox_activated(const QString&);
     void on_groupNameEdit_textEdited();
-    void on_lineEditChangePassConf_textChanged(const QString &arg1);
+    void on_lineEditChangePassConf_textChanged(const QString& arg1);
     void on_lineEditChangePass_textChanged();
     void on_mozillaRadioButton_clicked();
     void on_radioAutologinNo_clicked();
@@ -91,19 +92,17 @@ private slots:
     void on_sharedRadioButton_clicked();
     void on_syncRadioButton_clicked();
     void on_tabWidget_currentChanged();
-    void on_toUserComboBox_activated(const QString &arg1);
+    void on_toUserComboBox_activated(const QString& arg1);
     void on_userComboBox_activated(const QString&);
     void on_userComboMembership_activated(const QString&);
     void on_userNameEdit_textEdited();
-    void on_userPassword2Edit_textChanged(const QString &arg1);
+    void on_userPassword2Edit_textChanged(const QString& arg1);
     void on_userPasswordEdit_textChanged();
 
 private:
-    Cmd *shell;
+    Cmd* shell;
     QSettings settings;
     QStringList users;
-
 };
 
 #endif
-
